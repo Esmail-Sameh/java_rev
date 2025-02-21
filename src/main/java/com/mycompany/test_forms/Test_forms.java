10890b03058fe1531602c6023557253949b0b5ac
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-21 06:22:41 +0200

    add simple calc ( + - x ÷ %)

--- src/main/java/com/mycompany/test_forms/Test_forms.java
+++ src/main/java/com/mycompany/test_forms/Test_forms.java
@@ -7,7 +7,8 @@ public class Test_forms {
 
     public static void main(String[] args) {
         create_message_box_with_name frm = new create_message_box_with_name();
-        frm.setVisible(true);
-        frm.setLocationRelativeTo(null);
+        SimpleCalc calc = new SimpleCalc();
+        calc.setVisible(true);
+        calc.setLocationRelativeTo(null);
     }
 }
