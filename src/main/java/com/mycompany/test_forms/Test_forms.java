c9f7ad45fa1040e825b9c0f24924603e5c8cd6cb
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-21 07:12:53 +0200

    add Calculate date of birth

--- src/main/java/com/mycompany/test_forms/Test_forms.java
+++ src/main/java/com/mycompany/test_forms/Test_forms.java
@@ -6,9 +6,10 @@ package com.mycompany.test_forms;
 public class Test_forms {
 
     public static void main(String[] args) {
-        create_message_box_with_name frm = new create_message_box_with_name();
-        SimpleCalc calc = new SimpleCalc();
-        calc.setVisible(true);
-        calc.setLocationRelativeTo(null);
+//        create_message_box_with_name frm = new create_message_box_with_name();
+//        SimpleCalc calc = new SimpleCalc();
+        CalcDateOfBirth db = new CalcDateOfBirth();
+        db.setVisible(true);
+        db.setLocationRelativeTo(null);
     }
 }
