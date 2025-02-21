2937cbb6ae10ad21505ee16b04d80b2bd26a292e
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-21 05:29:51 +0200

    first commit and show message box with name

---
+++ src/main/java/com/mycompany/test_forms/Test_forms.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.test_forms;
+
+
+public class Test_forms {
+
+    public static void main(String[] args) {
+        create_message_box_with_name frm = new create_message_box_with_name();
+        frm.setVisible(true);
+        frm.setLocationRelativeTo(null);
+    }
+}
