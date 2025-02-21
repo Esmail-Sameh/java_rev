eba643ca41c0f94e5a63790dbf02de0200fd773c
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-21 22:18:05 +0200

    how to create file

--- src/main/java/com/mycompany/test_forms/Test_forms.java
+++ src/main/java/com/mycompany/test_forms/Test_forms.java
@@ -2,14 +2,29 @@
 
 package com.mycompany.test_forms;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
 
 public class Test_forms {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
 //        create_message_box_with_name frm = new create_message_box_with_name();
 //        SimpleCalc calc = new SimpleCalc();
-        CalcDateOfBirth db = new CalcDateOfBirth();
-        db.setVisible(true);
-        db.setLocationRelativeTo(null);
+//        CalcDateOfBirth db = new CalcDateOfBirth();
+//        db.setVisible(true);
+//        db.setLocationRelativeTo(null);
+        try{
+        PrintWriter w = new PrintWriter("Names.txt");
+        w.println("Esmail");
+        w.println("Ahmed");
+        w.println("Temer");
+        w.println("Mazen");
+        w.close();
+        }catch(FileNotFoundException ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+
     }
 }
